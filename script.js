26f035114ce31d6f5343bbda9782a05af1a2657a
Rihards Mac <Rihards.pauls07@gmail.com>
2024-11-08 09:43:15 +0200

    vairāk datu

--- script.js
+++ script.js
@@ -1,87 +1,116 @@
 const lauks = document.getElementById("lauks");
 const ctx = lauks.getContext("2d");
+let inputData = document.getElementById("inputData")
 ctx.font = "60px serif";
 
 ctx.fillStyle="green";
 
 
-
-//test
-//fons
-
-ctx.beginPath();
-ctx.fillRect(0, 0, 800, 700);
-ctx.stroke();
-
-ctx.fillStyle="gray";
-
-//ceļa līnija
-//līkums sākums
-ctx.beginPath();
-ctx.moveTo(0, 219);
-ctx.lineTo(300, 700);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.moveTo(47, 0);
-ctx.lineTo(500, 700);
-ctx.stroke();
-
-//pagrieziena beigas
-ctx.beginPath();
-ctx.moveTo(200, 19);
-ctx.lineTo(800, 100);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.moveTo(0, 152);
-ctx.lineTo(800, 270);
-ctx.stroke();
+let track_witdh=10000 //mlilimetri
+let proportion = 2.5 // 1 pix ir 2,5 mm , 10000/400
 
 
-//ceļa aizpildijums
-//sakums
-ctx.beginPath();
-ctx.moveTo(0, 219);
-ctx.lineTo(300, 700);
-ctx.lineTo(500, 700);
-ctx.lineTo(47, 0)
-ctx.lineTo(0, 0);
-ctx.fill();
+function drawTrack(){
+    ctx.beginPath();
+    ctx.fillRect(0, 0, 800, 700);
+    ctx.stroke();
+    ctx.fillStyle="gray";
+    //ceļa līnija
+    //līkums sākums
+    ctx.beginPath();
+    ctx.moveTo(0, 219);
+    ctx.lineTo(300, 700);
+    ctx.stroke();
 
-// pagrieziena vidus
-ctx.beginPath();
-ctx.arc(200, 200, 180, 1.81,  1.56* Math.PI);
-ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(47, 0);
+    ctx.lineTo(500, 700);
+    ctx.stroke();
 
-ctx.fillStyle = "green";
+    //pagrieziena beigas
+    ctx.beginPath();
+    ctx.moveTo(200, 19);
+    ctx.lineTo(800, 100);
+    ctx.stroke();
 
-ctx.beginPath();
-ctx.arc(200, 200, 20, 2.35,  4* Math.PI);
-ctx.fill(); 
-
-ctx.fillStyle = "gray";
-
-//beigas
-ctx.beginPath();
-ctx.moveTo(200, 20);
-ctx.lineTo(800, 100);
-ctx.lineTo(800, 270);
-ctx.lineTo(200, 180)
-ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(0, 152);
+    ctx.lineTo(800, 270);
+    ctx.stroke();
 
 
-//ceļa līniju vidus
-//pagrieziena vidus
-ctx.beginPath();
-ctx.arc(200, 200, 180, 2.7,  1.5* Math.PI);
-ctx.stroke();
+    //ceļa aizpildijums
+    //sakums
+    ctx.beginPath();
+    ctx.moveTo(0, 219);
+    ctx.lineTo(300, 700);
+    ctx.lineTo(500, 700);
+    ctx.lineTo(47, 0)
+    ctx.lineTo(0, 0);
+    ctx.fill();
 
-ctx.beginPath();
-ctx.arc(200, 200, 20, 2.35,  1.5* Math.PI);
-ctx.stroke();
+    // pagrieziena vidus
+    ctx.beginPath();
+    ctx.arc(200, 200, 180, 1.81,  1.56* Math.PI);
+    ctx.fill();
+
+    ctx.fillStyle = "green";
+
+    ctx.beginPath();
+    ctx.arc(200, 200, 20, 2.35,  4* Math.PI);
+    ctx.fill(); 
+    ctx.fillStyle = "gray";
+    //beigas
+    ctx.beginPath();
+    ctx.moveTo(200, 20);
+    ctx.lineTo(800, 100);
+    ctx.lineTo(800, 270);
+    ctx.lineTo(200, 180)
+    ctx.fill();
+    //ceļa līniju vidus
+    //pagrieziena vidus
+    ctx.beginPath();
+    ctx.arc(200, 200, 180, 2.7,  1.5* Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(200, 200, 20, 2.35,  1.5* Math.PI);
+    ctx.stroke();
+}
+function change(){
+    
+}
+
+let carData={
+    witdh:0,
+    length:0,
+    mass:0,
+    power:0,
+    pointOfMassX:0,
+    pointOfMassY:0,
+
+}
+
+let startPoint={
+    right: {
+        x:300 + (carData.witdh/2.5)/2,
+        y:700
+    },
+    mid:{
+        x:500,
+        y:700
+    },
+    left:{
+        x:700 - (carData.witdh/2.5)/2,
+        y:700
+    }
+}
+let endPoint ={
+    
+}
 
 
 
 
+
+drawTrack();
 //Pati programma
